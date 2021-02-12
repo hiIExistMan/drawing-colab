875f8b9a54044a97fc46b70e184f107356467e44
hiIExistMan <codymotes2010@gmail.com>
2021-02-12 16:55:44 -0500

    fixing problems in drawing program and it works now!

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
 const canvas = document.querySelector("canvas");
 let mouseDown = false;
-let {arc, fillRect, fillStyle, strokeStyle} = canvas.getContext("2d");
-fillStyle = "black";
-fillRect(0, 0, 640, 360);
+let mouseX = 0, mouseY = 0;
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 640, 360);
+ctx.fill();
 addEventListener("mousedown", () => mouseDown = true);
 addEventListener("mouseup", () => mouseDown = false);
+addEventListener("mousemove", e => {
+  mouseX = e.x;
+  mouseY = e.y;
+})
 
 setInterval(() => {
   if(mouseDown) {
-    fillStyle = "whitesmoke";
-    arc(mouseX, mouseY, 25, 0, Math.PI*2, false);
+    ctx.beginPath();
+    ctx.fillStyle = "whitesmoke";
+    ctx.arc(mouseX, mouseY, 10, 0, Math.PI*2, false);
+    ctx.fill();
   }
 }, 0);
 
