c453da4f4cefd6a6f0450805d52e1cad7c2191b3
hiIExistMan <codymotes2010@gmail.com>
2021-02-12 16:42:43 -0500

    making a drawing app

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
 const canvas = document.querySelector("canvas");
+let mouseDown = false;
 const {arc, fillRect, fillStyle, strokeStyle} = canvas.getContext("2d");
+fillStyle = "black";
+fillRect(0, 0, 640, 360);
+addEventListener("mousedown", () => mouseDown = true);
+addEventListener("mouseup", () => mouseDown = false);
 
 setInterval(() => {
-
+  if(mouseDown) {
+    fillStyle = "whitesmoke";
+    arc(mouseX, mouseY, 25, 0, Math.PI*2, false);
+  }
 }, 0);
 
 function fetchDrawing() {
