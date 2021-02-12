e81309bff6d90b48eccb3da5fa7b2ecb335f7e7f
hiIExistMan <codymotes2010@gmail.com>
2021-02-12 16:25:41 -0500

    add start files

---
+++ sketch.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector("canvas");
+const {arc, fillRect, fillStyle, strokeStyle} = canvas.getContext("2d");
+
+setInterval(() => {
+
+}, 0);
+
+function fetchDrawing() {
+  fetch();
+}
