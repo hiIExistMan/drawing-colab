72b0cebdb0b55d0b237f0524dbfbf8d388ffd3ef
hiIExistMan <codymotes2010@gmail.com>
2021-02-12 16:36:03 -0500

    adding helper functions add thinking about the server more

--- sketch.js
+++ sketch.js
@@ -6,5 +6,18 @@ setInterval(() => {
 }, 0);
 
 function fetchDrawing() {
-  fetch();
+  fetch("https://localhost:8000/drawing").then(async responce => {
+    const json = await responce.json();
+    const pixels = json.pixels;
+    
+  });
 }
+
+function setPixels(p) {
+  let id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+  let pixels = id.data;
+  pixels = p;
+  ctx.putImageData(id, 0, 0);
+}
+
+const getPixels = () => ctx.getImageData(0, 0, canvasWidth, canvasHeight).data;
